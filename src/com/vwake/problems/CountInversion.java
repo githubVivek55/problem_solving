0cfee447d5eb908d643a8e61f507f47a80fe8c11
githubVivek55 <vivekpce55@outlook.com>
2022-06-26 18:43:29 +0800

    Count inversion - complete solution

--- src/com/vwake/problems/CountInversion.java
+++ src/com/vwake/problems/CountInversion.java
@@ -1,61 +1,46 @@
 package com.vwake.problems;
 
+import java.util.Arrays;
+
 public class CountInversion {
     public static void main(String[] args) {
-        long[] arr = {10, 10, 10};
+        long[] arr = {2, 4, 1, 3, 5};
         long N = arr.length;
-        long res = inversionCount(arr,N);
-        System.out.println("Output is ="+res);
-    }
-    static long inversionCount(long arr[], long N)
-    {
-        long res=0;
-        sort(arr,0,(int)N-1,res);
-        return res;
-    }
-    static void sort(long[] arr, int l,int r,long res){
-        if(l<r){
-            int m = l+(r-l)/2;
-            sort(arr,l,m,res);
-            sort(arr,m+1,r,res);
-            merge(arr, l,m,r,res);
-        }
-    }
-    static void merge(long[] arr,int l, int m, int r,long res){
-        int n1 = m-l+1;
-        int n2 = r-m;
-        long[] L = new long[n1];
-        long[] R =new long[n2];
+        long ans=mergeSort(arr,new long[(int)N],0,N-1);
 
-        for(int i=0;i<n1;i++){
-            L[i]=arr[l+i];
+    }
+    static long mergeSort(long arr[],long temp[],long left,long right){
+        long mid=0, inv=0;
+        if(right>left){
+            mid=(left+right)/2;
+            inv+=mergeSort(arr,temp,left,mid);
+            inv+=mergeSort(arr,temp,mid+1,right);
+            inv+=merge(arr,temp,left,mid+1,right);
         }
+        return inv;
+    }
 
-        for(int i=0;i<n2;i++){
-            R[i]=arr[m+1+i];
-        }
-        int i=0,j=0;
-        int k=l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
+    static long merge(long arr[],long temp[],long left,long mid,long right){
+        long i,j,k,inv=0;
+        i=left;j=mid;k=left;
+        while((i<=mid-1) && (j<=right)){
+            if(arr[(int)i]<=arr[(int)j]){
+                temp[(int)k++]=arr[(int)i++];
             }
-            else {
-                arr[k] = R[j];
-                j++;
+            else{
+                temp[(int)k++]=arr[(int)j++];
+                inv+=(mid-i);
             }
-            k++;
         }
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+        while(i<=mid-1){
+            temp[(int)k++]=arr[(int)i++];
         }
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+        while(j<=right){
+            temp[(int)k++]=arr[(int)j++];
         }
+        for(i=(int)left;i<=right;i++){
+            arr[(int)i]=temp[(int)i];
+        }
+        return inv;
     }
 }

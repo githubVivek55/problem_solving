35d862dd344c93ee237c8436b4a17c4bcf5db2bb
githubVivek55 <vivekpce55@outlook.com>
2022-06-29 22:40:36 +0800

    Rain trapping water solution java

--- src/com/vwake/problems/TrappingRainWater.java
+++ src/com/vwake/problems/TrappingRainWater.java
@@ -2,21 +2,29 @@ package com.vwake.problems;
 
 public class TrappingRainWater {
     public static void main(String[] args) {
-        int[] arr = {8,8,2,4,5,5,1};
-        long result = solution(arr,arr.length);
+        int[] arr = {1, 1, 5, 2, 7, 6, 1, 4, 2, 3};
+        long result = solution(arr, arr.length);
         System.out.println(result);
     }
 
     public static long solution(int[] arr, int n) {
-        int max = arr[0];
-        long stg =0;
-        for(int i=1;i<n-1;i++){
-            if(arr[i]<max){
-                stg=stg+(max-arr[i]);
-            }else{
-                max=arr[i];
+        int left = 0;
+        int right = n - 1;
+        int leftMax = arr[left];
+        int rightMax = arr[right];
+        long result = 0;
+        while (left < right) {
+            if (leftMax <= rightMax) {
+                left++;
+                leftMax = Math.max(leftMax, arr[left]);
+                result += leftMax - arr[left];
+            }
+            if (leftMax > rightMax) {
+                right--;
+                rightMax = Math.max(rightMax, arr[right]);
+                result += rightMax - arr[right];
             }
         }
-        return stg;
+        return result;
     }
 }

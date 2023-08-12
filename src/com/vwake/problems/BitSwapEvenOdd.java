06f7988fbf7c9b4b947eebdc6eb96b3be13aac3d
githubVivek55 <vivekpce55@outlook.com>
2023-08-12 22:27:05 +0800

    Easy - 11/08 swap even odd bits

---
+++ src/com/vwake/problems/BitSwapEvenOdd.java
@@ -0,0 +1,24 @@
+package com.vwake.problems;
+
+public class BitSwapEvenOdd {
+    public static void main(String[] args) {
+        int n = 23;
+        int res = swapBits(n);
+        System.out.println(res);
+    }
+    static int swapBits(int x)
+    {
+        for (int i = 0; i < 32; i += 2) {
+            int i_bit = (x >> i) & 1; // find i th bit
+            int i_1_bit
+                    = (x >> (i + 1)) & 1; // find i+1 th bit
+            x = x - (i_bit << i) // remove i_bit
+                    - (i_1_bit << (i + 1)) // remove i+1 th bit
+                    + (i_bit
+                    << (i + 1)) // put i_bit at i+1 location
+                    + (i_1_bit
+                    << i); // put i+1 bit at i location
+        }
+        return x;
+    }
+}

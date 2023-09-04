02cfa6fb4ccca6fb5d4c7e564b04ba1c846f2a13
githubVivek55 <vivekpce55@outlook.com>
2023-09-04 20:54:07 +0800

    Easy - 04/09 Minimum number of coins

--- src/com/gfg/greedy/dynamicProgramming/MinNumberOfCoins.java
+++ src/com/gfg/greedy/dynamicProgramming/MinNumberOfCoins.java
@@ -1,7 +1,6 @@
 package com.gfg.greedy.dynamicProgramming;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MinNumberOfCoins {
@@ -13,10 +12,10 @@ public class MinNumberOfCoins {
     static List<Integer> minPartitions(int N) {
         int[] notes = {2000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
         List<Integer> result = new ArrayList<>();
-        int t = N,i=0;
-        while (t!=0 && i>=0){
-            if(t>=notes[i]){
-                t = t-notes[i];
+        int t = N, i = 0;
+        while (t != 0 && i >= 0) {
+            if (t >= notes[i]) {
+                t = t - notes[i];
                 result.add(notes[i]);
                 i--;
             }

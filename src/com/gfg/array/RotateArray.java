92026958b2ff91efc08772799496b21977b6184b
githubVivek55 <vivekpce55@outlook.com>
2024-05-07 07:54:50 +0800

    Easy - 07/05 Rotate array anti clockwise;

--- src/com/gfg/array/RotateArray.java
+++ src/com/gfg/array/RotateArray.java
@@ -9,22 +9,20 @@ public class RotateArray {
         System.out.println(Arrays.toString(arr));
     }
 
-    static void rotateArr(int arr[], int d, int n) {
+    static void rotateArr(int[] arr, int d, int n) {
         d = d % n;
         if(d == 0){
             return;
         }
         int[] arr2 = new int[d];
-        for (int i = 0; i < d; i++) {
-            arr2[i] = arr[i];
-        }
+        System.arraycopy(arr, 0, arr2, 0, d);
         int start = 0;
         for (int i = d; i < n; i++) {
             arr[start]=arr[i];
             start++;
         }
-        for (int i = 0; i < arr2.length; i++) {
-            arr[start] = arr2[i];
+        for (int j : arr2) {
+            arr[start] = j;
             start++;
         }
     }

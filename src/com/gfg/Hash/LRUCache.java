f6dcbccc25a84cf294c31c4d0934391aca981b89
githubVivek55 <vivekpce55@outlook.com>
2024-07-31 20:13:02 +0800

    Hard - 31/07 Implement LRU cache

--- src/com/gfg/Hash/LRUCache.java
+++ src/com/gfg/Hash/LRUCache.java
@@ -4,8 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LRUCache {
     private static int cap;

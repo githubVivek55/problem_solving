5a72ad0fecf81b5642d2c471e13919082ae8eb20
githubVivek55 <vivekpce55@outlook.com>
2024-10-27 12:12:26 +0800

    Basic - 27/10 Merge two string together

---
+++ src/com/gfg/strings/MergeString.java
@@ -0,0 +1,37 @@
+package com.gfg.strings;
+
+public class MergeString {
+    public static void main(String[] args) {
+        String one = "Hello";
+        String two = "Bye";
+        String merge = merge(one, two);
+        System.out.println(merge);
+    }
+
+    public static String merge(String S1, String S2) {
+        StringBuffer sb = new StringBuffer();
+        int i = 0, j = 0;
+
+        while (i < S1.length() && j < S2.length()) {
+            sb.append(S1.charAt(i));
+            sb.append(S2.charAt(j));
+            i++;
+            j++;
+        }
+
+        if (i < S1.length()) {
+            while (i < S1.length()) {
+
+                sb.append(S1.charAt(i));
+                i++;
+            }
+        }
+        if (j < S2.length()) {
+            while (j < S2.length()) {
+                sb.append(S2.charAt(j));
+                j++;
+            }
+        }
+        return sb.toString();
+    }
+}
